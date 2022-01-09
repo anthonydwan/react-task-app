8359e91c3d0184e29d265d5ac1792ec44a9a2b09
Anthony <anthonydwan@outlook.com>
2022-01-09 23:11:43 +1100

    adding core files in

---
+++ task-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Overview from './components/Overview';
+
+function App() {
+  return (
+    <div className="App">
+      <Overview/>
+    </div>
+  );
+}
+
+export default App;
---
+++ task-app/src/components/Overview.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const [state, setstate] = useState(initialState);
+function Overview() {
+  return <div>Hey</div>;
+}
+
+export default Overview;
